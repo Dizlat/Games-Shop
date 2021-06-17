5482c336708de3135e50a2b85fd7d678edda312b
Dizlat <dizlat.ur@gmail.com>
2021-06-18 03:37:43 +0600

    forms

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,24 @@
-
 from django import forms
 
 from .models import *
+
+
+class AddPostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['tittle', 'description', 'image', 'tags']
+
+    def __init__(self, *args, **kwargs):
+        self.request = kwargs.pop('request')
+        super(AddPostForm, self).__init__(*args, **kwargs)
+
+    def save(self):
+        data = self.cleaned_data
+        data['user'] = self.request.user
+        tags = data.pop('tags')
+        post = Post.objects.create(**data)
+        post.tags.add(*tags)
+        return post
 
 
 
